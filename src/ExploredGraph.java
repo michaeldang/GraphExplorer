9d17d44208af3c6b22d1bfe2eca523d8e2b93c9a
Michael Dang <michael.w.dang@gmail.com>
2014-11-26 14:44:57 -0800

    Added basic documentation

--- src/ExploredGraph.java
+++ src/ExploredGraph.java
@@ -4,8 +4,11 @@ import java.util.*;
 import java.util.function.Function;
 
 /**
- * @author your name(s) here.
- * Extra Credit Options Implemented, if any:  (mention them here.)
+ * @author Michael Dang mwldang@uw.edu
+ * Extra Credit Options Implemented, if any:  A5E1
+ *
+ * This program is used to solve the popular game called Towers of Hanoi. This program allows the user to solve
+ * the game through two different searching algorithms, depth first search and breadth first search.
  *
  * Solution to Assignment 5 in CSE 373, Autumn 2014
  * University of Washington.
@@ -31,6 +34,9 @@ public class ExploredGraph {
         initialize();
     }
 
+    /**
+     * Resets the Towers of Hanoi game configuration
+     */
     public void initialize() {
         Ve = new LinkedHashSet<Vertex>();
         Ee = new LinkedHashSet<Edge>();
@@ -40,6 +46,10 @@ public class ExploredGraph {
         setOperators();
     }
 
+
+    /**
+     * Constructs the different possible moves for the Towers of Hanoi game
+     */
     private void setOperators() {
         // (i, j) = {(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1)}
         operators = new ArrayList<Operator>();
@@ -52,14 +62,27 @@ public class ExploredGraph {
         }
     }
 
+    /**
+     * Returns the number of vertices explored when performing the current search
+     * @return The number of vertices explored when performing the current search
+     */
     public int nvertices() {
         return VeSize;
     }
 
+    /**
+     * Returns the number of edges created when performing the current search
+     * @return The number of edges created when performing the current search
+     */
     public int nedges() {
         return EeSize;
     }
 
+    /**
+     * Completes the Towers of Hanoi game using a depth first search approach
+     * @param vi The Vertex containing the starting configuration of the disks
+     * @param vj The Vertex containing the finished configuration of the disks
+     */
     public void dfs(Vertex vi, Vertex vj) {
         initialize();
         addNewVertex(vi);
@@ -67,6 +90,12 @@ public class ExploredGraph {
         dfsHelper(vi, vj);
     }
 
+    /**
+     * Helper method used to complete Towers of Hanoi using the depth first search approach
+     * @param currVertex The Vertex containing the current configuration of the disks
+     * @param vj The Vertex containing the finished configuration of the disks
+     * @return Whether the game has reached the end disk configuration
+     */
     private boolean dfsHelper(Vertex currVertex, Vertex vj) {
         if (currVertex != vj) {
             boolean foundSolution = false;
@@ -87,6 +116,11 @@ public class ExploredGraph {
         }
     }
 
+    /**
+     * Completes the Towers of Hanoi game using a breadth first search approach
+     * @param vi The Vertex containing the starting configuration of the disks
+     * @param vj The Vertex containing the finished configuration of the disks
+     */
     public void bfs(Vertex vi, Vertex vj) {
         initialize();
         Queue<Vertex> verticesToExplore = new LinkedList<Vertex>();
@@ -114,20 +148,34 @@ public class ExploredGraph {
         }
     }
 
-    private void addNewVertex(Vertex currVertex) {
-        Ve.add(currVertex);
+    /**
+     * Adds a new Vertex to list of vertices explored
+     * @param newVertex the Vertex that is being explored
+     */
+    private void addNewVertex(Vertex newVertex) {
+        Ve.add(newVertex);
         VeSize++;
     }
 
+    /**
+     * Creates a new Edge
+     * @param currVertex The current Vertex containing the current disk configuration
+     * @param newVertex The new Vertex containing the new disk configuration
+     */
     private void addNewEdge(Vertex currVertex, Vertex newVertex) {
         Edge newEdge = new Edge(currVertex, newVertex);
         Ee.add(newEdge);
         EeSize++;
         LinkedList<Edge> newVertexPath = (LinkedList<Edge>)map.get(currVertex).clone();
-        newVertexPath.add(newEdge);
-        map.put(newVertex, newVertexPath);
+        newVertexPath.add(newEdge); //Creates the path to the new Vertex
+        map.put(newVertex, newVertexPath); //Maps the new path to the new Vertex
     }
 
+    /**
+     * Finds the list of steps taken to get from the start Vertex to the end Vertex
+     * @param vj The Vertex containing the finished configuration of the disks
+     * @return The list of steps taken to get from the start Vertex to the end Vertex
+     */
     public ArrayList<Vertex> retrievePath(Vertex vj) {
         ArrayList<Vertex> path = new ArrayList<Vertex>();
         LinkedList<Edge> vjPath = map.get(vj);
@@ -138,35 +186,50 @@ public class ExploredGraph {
         return path;
     }
 
+    /**
+     * Finds the shortest path to get from the start Vertex to the end Vertex
+     * @param vi The Vertex containing the starting configuration of the disks
+     * @param vj The Vertex containing the finished configuration of the disks
+     * @return The list of steps taken to get from the start Vertex to the end Vertex
+     */
     public ArrayList<Vertex> shortestPath(Vertex vi, Vertex vj) {
         // TODO: return a shortest path as an array list
         bfs(vi, vj);
-        ArrayList<Vertex> path = retrievePath(vj);
-        return path;
+        return retrievePath(vj);
     }
 
+    /**
+     * The set of vertices explored when performing the current search
+     * @return The set of vertices explored when performing the current search
+     */
     public Set<Vertex> getVertices() {
         return Ve;
     }
 
+    /**
+     * The set of edges created when performing the current search
+     * @return The set of edges created when performing the current search
+     */
     public Set<Edge> getEdges() {
         return Ee;
     }
 
     public static void main(String[] args) {
         ExploredGraph eg = new ExploredGraph();
-//        Vertex v0 = eg.new Vertex("[[4,3,2,1],[],[]]");
-//        Vertex v1 = eg.new Vertex("[[],[],[4,3,2,1]]");
-//        eg.shortestPath(v0, v1);
-//        ArrayList<Vertex> answerPath = eg.retrievePath(v1);
-//        for (Vertex vertex: answerPath) {
-//            System.out.println(vertex);
-//        }
-
         Vertex v0 = eg.new Vertex("[[4,3,2,1],[],[]]");
         Vertex v1 = eg.new Vertex("[[],[],[4,3,2,1]]");
-        eg.dfs(v0, v1);
+        eg.shortestPath(v0, v1);
         ArrayList<Vertex> answerPath = eg.retrievePath(v1);
+        for (Vertex vertex: answerPath) {
+            System.out.println(vertex);
+        }
+        System.out.println("VeSize: " + eg.VeSize);
+        System.out.println("EeSize: " + eg.EeSize);
+
+        v0 = eg.new Vertex("[[4,3,2,1],[],[]]");
+        v1 = eg.new Vertex("[[],[],[4,3,2,1]]");
+        eg.dfs(v0, v1);
+        answerPath = eg.retrievePath(v1);
         for (Vertex vertex: answerPath) {
             System.out.println(vertex);
         }
@@ -204,7 +267,11 @@ public class ExploredGraph {
                 }
             }
         }
-        
+
+        /**
+         * Constructor that copies the information of the given Vertex into the current Vertex
+         * @param vertex The Vertex that the current Vertex copies information from
+         */
         public Vertex(Vertex vertex) {
             pegs = new Stack[vertex.pegs.length];
             for (int index = 0; index < vertex.pegs.length; index++) {
@@ -212,6 +279,10 @@ public class ExploredGraph {
             }
         }
 
+        /**
+         * Creates a String representation of the given Vertex
+         * @return A String representation of the given Vertex
+         */
         public String toString() {
             String ans = "[";
             for (int i = 0; i < NUMBER_OF_PEGS; i++) {
@@ -225,56 +296,79 @@ public class ExploredGraph {
         }
         
         @Override
+        /**
+         * Checks to see if the given object is equivalent to the current Vertex
+         */
         public boolean equals(Object v) {
-            if (v instanceof Vertex)
-                return hashCode() == v.hashCode();
-            else
-                return false;
+            return (v instanceof Vertex) && (hashCode() == v.hashCode());
         }
 
         @Override
+        /**
+         * Creates a hashcode for the current Vertex
+         */
         public int hashCode() {
             return toString().hashCode();
         }
     }
 
     class Edge {
-        public Vertex vi;
-        public Vertex vj;
+        public Vertex vi; //The starting disk configuration
+        public Vertex vj; //The disk configuration after a move has been performed
 
+        /**
+         * Constructs an Edge using the given vertices
+         * @param vi The starting disk configuration
+         * @param vj The disk configuration after a move has been performed
+         */
         public Edge(Vertex vi, Vertex vj) {
             this.vi = vi;
             this.vj = vj;
         }
 
+        /**
+         * Creates a String representation of the given edge
+         * @return A String representation of the given edge
+         */
         public String toString() {
             return vi + " -> " + vj;
         }
 
         @Override
-        public boolean equals(Object e) {
-            if (e instanceof Edge)
-                return hashCode() == e.hashCode();
-            else
-                return false;
-        }
+        /**
+         * Checks to see if the given object is equivalent to the current Edge
+         */
+        public boolean equals(Object e) { return (e instanceof Edge) && (hashCode() == e.hashCode()); }
 
         @Override
+        /**
+         * Creates a hashcode for the current Edge
+         */
         public int hashCode() {
             return vi.hashCode() + vj.hashCode();
         }
     }
 
+    /**
+     * A
+     */
     class Operator {
-        private int i, j;
+        private int i, j; //i = the peg you're taking a disk from. j = the peg you're putting the disk onto
 
+        /**
+         * Constructs an operator with the given peg information
+         * @param i The peg you're taking a disk from
+         * @param j The peg you're putting the disk onto
+         */
         public Operator(int i, int j) {
             this.i = i;
             this.j = j;
         }
 
-        // Additional explanation of what to do here will be given in GoPost or
-        // as extra text in the spec.
+        /**
+         * Checks to see if the current Operator can be performed on the given disk configuration (Vertex)
+         * @return Whether the current Operator's move can be performed on the given Vertex
+         */
         @SuppressWarnings("rawtypes")
         Function<Vertex, Boolean> getPrecondition() {
             return new Function<Vertex, Boolean>() {
@@ -291,6 +385,10 @@ public class ExploredGraph {
             };
         }
 
+        /**
+         * Performs the current Operator's move on the given Vertex
+         * @return The resulting disk configuration (Vertex) after the current Operator's move has been performed
+         */
         @SuppressWarnings("rawtypes")
         Function<Vertex, Vertex> getTransition() {
             return new Function<Vertex, Vertex>() {
@@ -304,6 +402,10 @@ public class ExploredGraph {
             };
         }
 
+        /**
+         * Creates a string representation of the current Operation
+         * @return A string representation of the current Operation
+         */
         public String toString() {
             // TODO: return a string good enough
             // to distinguish different operators
